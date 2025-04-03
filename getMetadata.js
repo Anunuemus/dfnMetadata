f0828822d93f7cf9d3eb52b63bc2f45ff974256b
Funktionsaccount IT-Labor9.5 <itlabor95@pad.ptb.de>
2025-04-03 10:45:41 +0200

    pls work

---
+++ getMetadata.js
@@ -0,0 +1,288 @@
+const http = require('http');
+const https = require('https');
+const {HttpsProxyAgent} = require('https-proxy-agent');
+const { DOMParser } = require('xmldom');
+const fs = require('fs');
+const crypto = require('crypto');
+const { createHash } = require('crypto');
+const { SignedXml } = require('xml-crypto');
+const path = require('path');
+
+function writeFiles(match, jsonString, certStr, pubKeyStr){
+
+    const hash = createHash('sha256');    
+    hash.update(jsonString, 'utf8');
+    const digest = hash.digest('hex');
+
+    let fileames = fs.readdirSync('SPs');
+    let file = fileames.find(file => file.includes(match.name));    
+    let name = match.name.concat('-', digest)
+
+    if(file){
+
+        if(!file.includes(name)){
+
+            fs.writeFileSync('SPs/'.concat(file), jsonString);
+
+            fs.renameSync('SPs/'.concat(file), `SPs/${name}-metadata.json`);
+        
+            fs.writeFileSync(`SPs/${match.name}-sp-e.bs.ptb.de.crt`, certStr);
+        
+            fs.writeFileSync(`SPs/${match.name}-sp-e.bs.ptb.de.pub`, pubKeyStr);
+        }
+
+    }else{
+
+        fs.writeFileSync(`SPs/${name}-metadata.json`, jsonString);
+    
+        fs.writeFileSync(`SPs/${match.name}-sp-e.bs.ptb.de.crt`, certStr);
+    
+        fs.writeFileSync(`SPs/${match.name}-sp-e.bs.ptb.de.pub`, pubKeyStr);
+    }
+}
+
+function deleteFiles(names){
+
+    const EXTENSION = '.json';
+
+    const targetFiles = fs.readdirSync('SPs').filter(file => {
+        return path.extname(file) === EXTENSION;
+    });
+
+    targetFiles.forEach(file => {
+
+        if(!names.some(name => file.includes(name))){
+
+            fs.unlink('SPs/'.concat(file), (err) => {
+                if (err) throw err;
+            });
+
+            const fileName = file.toString().split('-')[0];
+
+            fs.unlink(`SPs/${fileName}-sp-e.bs.ptb.de.crt`, (err) => {
+                if (err) console.log(err);
+            });
+
+            fs.unlink(`SPs/${fileName}-sp-e.bs.ptb.de.pub`, (err) => {
+                if (err) console.log(err);
+            });
+        }
+    });
+    
+}
+
+function validateSig(xmlDoc, xmlString){
+
+    const node = xmlDoc.getElementsByTagName('ds:Signature')[0];
+    
+    const sig = new SignedXml({
+        publicCert:  fs.readFileSync('dfn-aai.pem', 'utf8'),
+        getCertFromKeyInfo: () => null,
+    });
+    sig.loadSignature(node);
+
+    try{
+        const isValid = sig.checkSignature(xmlString.concat("test"));
+        if(!isValid){
+            throw new Error('Bad Signature or metadata.');
+        }
+    }catch (e){
+        console.log(e);
+    }
+}
+
+function getCertificate(descriptor) {
+
+    const body = descriptor.getElementsByTagName('ds:X509Certificate');
+
+    if(body.length === 2 && !body[0].getAttribute('use').includes('signing') && !body[0].getAttribute('use').includes('encryption') && !body[1].getAttribute('use').includes('signing') && !body[1].getAttribute('use').includes('encryption') && descriptor.getElementsByTagName('ds:KeyName')[0].textContent === descriptor.getElementsByTagName('ds:KeyName')[0].textContent && body[0].textContent !== body[1].textContent){
+        
+        const c1 = `-----BEGIN CERTIFICATE-----\n${body[0].textContent}\n-----END CERTIFICATE-----`;
+        const cert1 = new crypto.X509Certificate(c1);
+        
+        const c2 = `-----BEGIN CERTIFICATE-----\n${body[1].textContent}\n-----END CERTIFICATE-----`;
+        const cert2 = new crypto.X509Certificate(c2);
+
+        // console.log(cert1.validTo + ' - ' + cert2.validTo);
+        // console.log(cert1.validToDate > cert2.validToDate ? 'cert1' : 'cert2');
+        
+        const ret =  cert1.validToDate > cert2.validToDate ? cert1 : cert2;
+        
+        if(Date.parse(ret.validTo) < new Date().getTime()){
+            throw new Error('Certificate is not valid anymore');
+        }
+
+        return ret;
+    }
+
+    const certificate = `-----BEGIN CERTIFICATE-----\n${body[0].textContent}\n-----END CERTIFICATE-----`;
+    const ret = new crypto.X509Certificate(certificate);
+    
+    if(Date.parse(ret.validTo) < new Date().getTime()){
+        throw new Error('Certificate is not valid anymore');
+    }
+
+    return ret;
+}
+
+async function getLogo(descriptor) {
+
+    const logoElement = descriptor.getElementsByTagName('mdui:Logo')[0].textContent;
+    
+    if(logoElement){    
+        const agent = new HttpsProxyAgent('http://webproxy.bs.ptb.de:8080');
+        
+        return new Promise((resolve, reject) => {
+
+            https.get(logoElement, { agent }, (resp) => {
+                
+                resp.setEncoding('base64');
+                let body = "data:" + resp.headers["content-type"] + ";base64,";
+
+                resp.on('data', (data) => {
+                    body += data;
+                });
+
+                resp.on('end', () => {
+                    resolve(body);
+                });
+            }).on('error', (e) => {
+                reject(`Got error: ${e.message}`);
+            });
+        });
+    }
+    return "";
+}
+
+function getAssertionConsumerService(descriptor){
+    const body = descriptor.getElementsByTagName('AssertionConsumerService')[0];
+    return body.getAttribute("Location");
+}
+
+function getContact(entity){
+    const body = entity.getElementsByTagName('ContactPerson');
+    return Array.from(body).map(contact => 
+        [contact.getAttribute('contactType'), contact.getElementsByTagName('EmailAddress')[0].textContent.split(':')[1]]
+    );
+}
+
+function getRequestedAttributes(descriptor){
+
+    const attributes = descriptor.getElementsByTagName('RequestedAttribute');
+    const attributeArray = Array.from(attributes);
+
+    return attributeArray.filter(attribute => 
+        attribute.getAttribute("isRequired") === "true"
+    ).map(attribute => 
+        attribute.getAttribute("FriendlyName")
+    );
+}
+
+async function createJSON(sp) {
+
+    const xmlString = fs.readFileSync('metadata.xml', 'utf8').toString();
+
+    const xmlDoc = new DOMParser().parseFromString(xmlString, 'application/xml');
+
+    const entities = xmlDoc.getElementsByTagName('EntityDescriptor');
+
+    validateSig(xmlDoc, xmlString);
+
+    for (let i = 0; i < entities.length; i++) {
+
+        const entity = entities[i];
+        const entityID = entity.getAttribute('entityID');
+
+        const match = sp.find(ID => entityID.includes(ID.entityID));
+    
+        if (match) {
+
+            let certStr = "";
+            let pubKeyStr = "";
+            const descriptor = entity.getElementsByTagName('SPSSODescriptor')[0];
+    
+            const cert = getCertificate(descriptor);
+            const publicKey = cert.publicKey.export({ type: 'spki', format: 'pem' });
+            const logo = await getLogo(descriptor);
+            const assertionConsumerService = getAssertionConsumerService(descriptor);
+            const contacts = getContact(entity);
+            const requestedAttributes = getRequestedAttributes(descriptor);
+    
+            const results = {
+                appl: match.name,
+                issuer: match.entityID,
+                url: "https://sp-e.bs.ptb.de:8446",
+                logoUrl: logo,
+                protection_requirements: "normal",
+                users: [], //müsste man schauen wer da rein soll                
+                saml_ecp: {
+                    allowed: false,
+                    service_accounts: [],
+                    allowed_user: false,
+                },
+                groups: [],
+                preferredAuthnContext: "urn:oasis:names:tc:SAML:2.0:ac: classes:Kerberos",
+                preferredNameidFormat: "urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified",
+                singleLogoutService: "",
+                logoutResponseUrl: "",
+                assertionConsumerService: assertionConsumerService,
+                sendErrorResponse: true,
+                visible: true,
+                secure: false,
+                cert: `./SPs/${match.name}-sp-e.bs.ptb.de.crt`,
+                pub: `./SPs/${match.name}-sp-e.bs.ptb.de.pub`,
+                oidcClient: {
+                    client_id: "",
+                    client_secret: "",
+                    redirect_uris: [],
+                    post_logout_redirect_uris: [],
+                    backchannel_logout_uri: "",
+                    backchannel_logout_session_required: true,
+                    response_types: [],
+                    grant_types: [],
+                    token_endpoint_auth_method: "",
+                    id_token_signed_response_alg: "",
+                    pkce: true
+                },
+    
+                //reqAttr: requestedAttributes
+                //contacts : contacts
+            };
+            
+            certStr = certStr.concat(cert.toString());
+            pubKeyStr = pubKeyStr.concat(publicKey.toString());
+
+            const jsonString = JSON.stringify(results, null, 2);
+
+            writeFiles(match, jsonString, certStr, pubKeyStr);
+        }
+    }
+    
+    deleteFiles(sp.map(entry => entry.name));
+}
+
+function getMetadata(){
+
+    const json = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+
+    const spMetadataUrl = json.spMetadataUrl;
+
+    const sp = json.sp;
+    createJSON(sp);
+
+    // const agent = new HttpsProxyAgent('http://webproxy.bs.ptb.de:8080');
+    // const req = http.get(spMetadataUrl, {agent},  (res) => {
+    //     const fileStream = fs.createWriteStream('metadata.xml');  
+    
+    //     res.pipe(fileStream);
+    
+    //     fileStream.on('finish', () => {
+    //         createJSON(sp);
+    //     });
+    // });
+
+    // req.on('error', (err) => console.error('Error:', err));
+    // req.end();
+}
+
+getMetadata();
